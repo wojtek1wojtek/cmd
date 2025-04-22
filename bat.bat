f91055de32fa80a9d9690f2a33f852ff1390fd1a
wojtek1wojtek <77987242+wojtek1wojtek@users.noreply.github.com>
2025-04-22 13:46:45 +0200

    Add files via upload

---
+++ bat.bat
@@ -0,0 +1,79 @@
+echo off
+cls
+echo 3C/ 2DV F3 FJ2BAF
+timeout 1 >nul
+cls
+echo Y5. 4DS J3 NN45AF
+timeout 1 >nul
+cls
+echo Yof nE4 C4 7NSQAB
+timeout 1 >nul
+cls
+echo You nE4 C4 7NSQAB
+timeout 1 >nul
+cls
+echo You a3Q 6G DJK238
+timeout 1 >nul
+cls
+echo You arD jG 38EW24
+timeout 1 >nul
+cls
+echo You are Dw F39siA
+timeout 1 >nul
+cls
+echo You are aw 253A73
+timeout 1 >nul
+cls
+echo You are an f34729
+timeout 1 >nul
+cls
+echo You are an Ir82ea
+timeout 1 >nul
+cls
+echo You are an Id3456
+timeout 1 >nul
+cls
+echo You are an Idihd4
+timeout 1 >nul
+cls
+echo You are an Idio45
+timeout 1 >nul
+cls
+echo You are an Idiot7
+timeout 1 >nul
+cls
+echo You are an Idiot!
+timeout 1 >nul
+echo :)  :)  :)
+color 70
+timeout 1 >nul
+color 07
+timeout 1 >nul
+color 70
+timeout 1 >nul
+color 07
+timeout 1 >nul
+color 70
+timeout 1 >nul
+color 07
+timeout 1 >nul
+color 70
+timeout 1 >nul
+color 07
+timeout 1 >nul
+color 70
+timeout 1 >nul
+color 07
+timeout 1 >nul
+color 70
+timeout 1 >nul
+color 07
+timeout 1 >nul
+color 70
+timeout 1 >nul
+color 07
+timeout 1 >nul
+color 70
+timeout 1 >nul
+color 07
+timeout 1 >nul
